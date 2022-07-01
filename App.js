c92a5a65240a3f668f98757bbe5c1c92d2a8300b
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-07-01 19:15:41 +0700

    08-Other-Navigators-Starting-Project
    08-Other-Navigators-Starting-Project

--- App.js
+++ App.js
@@ -1,50 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import MealDetailScreen from './screens/MealDetailScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="light" />
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: '#351401' },
-            headerTintColor: 'white',
-            contentStyle: { backgroundColor: '#3f2f25' },
-          }}
-        >
-          <Stack.Screen
-            name="MealsCategories"
-            component={CategoriesScreen}
-            options={{
-              title: 'All Categories',
-            }}
-          />
-          <Stack.Screen
-            name="MealsOverview"
-            component={MealsOverviewScreen}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId,
-            //   };
-            // }}
-          />
-          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
+  <NavigationContainer>
+    <Drawer.Navigator initialRouteName='Welcome'>
+      <Drawer.Screen name='Welcome' component={WelcomeScreen} />
+      <Drawer.Screen name='User' component={UserScreen} />
+    </Drawer.Navigator>
+  </NavigationContainer>
+  ) ;
 }
-
-const styles = StyleSheet.create({
-  container: {},
-});
